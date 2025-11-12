3e28a7262ad2cbf52523ec1e5831848a3fa8b929
hello-ai-bunny <hello@ai-bunny.ru>
2025-11-12 21:33:01 +0500

    добавила дб и таблицу юзера как в тз

---
+++ backend/app/data/db.py
@@ -0,0 +1,37 @@
+import os
+from contextlib import  contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tinterest.db")
+
+engine = create_engine(DATABASE_URL, echo=False, check_same_thread=False)
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=Session,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+class Base(DeclarativeBase):
+    pass
+
+@contextmanager
+def db_conn():
+    db = SessionLocal()
+    try:
+        yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
