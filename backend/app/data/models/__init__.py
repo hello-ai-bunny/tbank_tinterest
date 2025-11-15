9bf3681348956b6109360c14ff1524b0c332f7c3
hello-ai-bunny <hello@ai-bunny.ru>
2025-11-15 15:04:56 +0500

    создала миграцию алембик и проверила через создание локальной скульлайт базы

---
+++ backend/app/data/models/__init__.py
@@ -0,0 +1,15 @@
+from .user import User
+from .profile import Profile
+from .interest import Interest
+from .user_interest import UserInterest
+from .chat import Chat
+from .message import Message
+
+__all__ = [
+    "User",
+    "Profile",
+    "Interest",
+    "UserInterest",
+    "Chat",
+    "Message",
+]
