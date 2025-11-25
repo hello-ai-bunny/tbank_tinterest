97f0904cd7ac0d14f05967a7b336d2a1f8ff5d9b
hello-ai-bunny <hello@ai-bunny.ru>
2025-11-25 16:08:46 +0500

    добавила ручку работы с интересами

---
+++ backend/app/api/routers/survey.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from ...dependencies.auth import get_current_user
+from ...data.models.user import User
+from ...schemas.survey_schemas import InterestBase
+from ...services import survey_service
+
+router = APIRouter()
+
+
+@router.get("/interests", response_model=list[InterestBase])
+def get_all_interests():
+    return survey_service.get_all_interests()
+
+
+@router.get("/me/interests", response_model=list[InterestBase])
+def get_my_interests(current_user: User = Depends(get_current_user)):
+    return survey_service.get_user_interests(user_id=current_user.id)
+
+
+@router.put("/me/interests", response_model=list[InterestBase])
+def set_my_interests(
+    interests_ids: list[str], current_user: User = Depends(get_current_user)
+):
+    return survey_service.replace_user_interests(
+        user_id=current_user.id, interests_ids=interests_ids
+    )
