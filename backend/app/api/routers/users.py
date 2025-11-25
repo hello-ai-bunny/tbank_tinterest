ffabc1ba4f8e5d9103a0b85f38d04594497af6c3
hello-ai-bunny <hello@ai-bunny.ru>
2025-11-25 15:47:17 +0500

    добавила работу с профилем

---
+++ backend/app/api/routers/users.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from ...dependencies.auth import get_current_user
+from ...data.models.user import User
+from ...schemas.user_schemas import UserResponse, ProfileUpdate
+from ...services import user_service
+
+router = APIRouter()
+
+
+@router.get("/me", response_model=UserResponse)
+def read_users_me(current_user: User = Depends(get_current_user)):
+    user_with_profile = user_service.get_user_with_profile(user_id=current_user.id)
+
+    return user_with_profile
+
+@router.patch("/me", response_model=UserResponse)
+def update_user_profile(profile_data: ProfileUpdate, current_user: User = Depends(get_current_user)):
+    udpated_profile = user_service.update_profile(
+        user_id=current_user.id, profile_data=profile_data
+    )
+
+    if not udpated_profile:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Профиль не найден"
+        )
+    
+    updated_user = user_service.get_user_with_profile(user_id=current_user.id)
+    return updated_user
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,7 +1,8 @@
 from ..data.db import db_conn
 from ..data.models.user import User
 from ..data.models.profile import Profile
-from ..schemas.user_schemas import UserLogin
+from ..schemas.user_schemas import UserLogin, ProfileBase, ProfileUpdate
+from sqlalchemy.orm import joinedload
 
 
 def create_user(user_data: UserLogin) -> User:
@@ -20,3 +21,27 @@ def create_user(user_data: UserLogin) -> User:
 def get_user_by_login(login: str) -> User | None:
     with db_conn() as db:
         return db.query(User).filter(User.email == login).first()
+
+
+def get_user_with_profile(user_id: str) -> User | None:
+    with db_conn() as db:
+        return (
+            db.query(User)
+            .options(joinedload(User.profile))
+            .filter(User.id == user_id)
+            .first()
+        )
+
+
+def update_profile(user_id: str, profile_data: ProfileUpdate) -> Profile | None:
+    with db_conn() as db:
+        profile = db.query(Profile).filter(Profile.user_id == user_id).first()
+        if not profile:
+            return None
+
+        update_data = profile_data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(profile, key, value)
+
+        db.add(profile)
+        return profile
