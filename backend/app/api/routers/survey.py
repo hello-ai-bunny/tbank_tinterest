c59ede0a89171e91c58360af9bcc3391210df7b9
hello-ai-bunny <hello@ai-bunny.ru>
2025-12-02 13:04:45 +0500

    добавила немного валидации в поля юзера и добавила проверку на минимум 1 интерес при смене интересов

--- backend/app/api/routers/survey.py
+++ backend/app/api/routers/survey.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from ...dependencies.auth import get_current_user
 from ...data.models.user import User
 from ...schemas.survey_schemas import InterestBase
@@ -21,6 +21,11 @@ def get_my_interests(current_user: User = Depends(get_current_user)):
 def set_my_interests(
     interests_ids: list[str], current_user: User = Depends(get_current_user)
 ):
+    if not interests_ids:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Необходимо выбрать хотя бы один интерес.",
+        )
     return survey_service.replace_user_interests(
         user_id=current_user.id, interests_ids=interests_ids
     )
